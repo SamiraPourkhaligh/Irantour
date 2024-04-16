066a68a9379f09ccf70e283125995846a1e0a40e
SamiraPorkhaligh <samira.pourkhaligh@gmail.com>
2024-04-16 13:42:13 +0330

    Create search form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Show and hide menu button
 let navbar = document.querySelector("header .navbar");
 
 document.querySelector("#menu-btn").onclick = () => {
@@ -6,6 +7,17 @@ document.querySelector("#menu-btn").onclick = () => {
 document.querySelector("#nav-close").onclick = () => {
   navbar.classList.remove("active");
 };
+
+// Show and hide search form
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.add("active");
+};
+document.querySelector("#search-close").onclick = () => {
+  searchForm.classList.remove("active");
+};
+
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
