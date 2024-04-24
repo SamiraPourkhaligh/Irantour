acf472468835fb2a4983888ca982b8e7d25b2bb1
samiraPourkhaligh <samira.pourkhaligh@gmail.com>
2024-04-24 14:23:03 +0330

    Create shop section

--- js/script.js
+++ js/script.js
@@ -37,12 +37,40 @@ window.onload = () => {
   }
 };
 
-// swiper slide
+// swiper slide for hom section
 var swiper = new Swiper(".home-slider", {
   loop: true,
   grabCursor: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+// swiper slide for product section
+var swiper = new Swiper(".product-slider", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1440: {
+      slidesPerView: 5,
+    },
   },
 });
